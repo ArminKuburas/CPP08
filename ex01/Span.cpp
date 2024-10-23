29e3d76af76d339a3bedd7a586e831a39bcf745c
ArminKuburas <akuburass@gmail.com>
2024-10-23 23:57:32 +0300

    feat: span done for the most part

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -6,7 +6,85 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 23:39:44 by akuburas          #+#    #+#             */
-/*   Updated: 2024/10/23 23:39:45 by akuburas         ###   ########.fr       */
+/*   Updated: 2024/10/23 23:57:17 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Span.hpp"
+
+Span::Span() : _n(0)
+{
+}
+
+Span::Span(unsigned int n) : _n(n)
+{
+}
+
+Span::Span(const Span& copy)
+{
+	*this = copy;
+}
+
+Span::~Span()
+{
+}
+
+Span& Span::operator=(const Span& obj)
+{
+	if (this == &obj)
+		return *this;
+	_n = obj._n;
+	_numbers = obj._numbers;
+	return *this;
+}
+
+void Span::addNumber(int number)
+{
+	if (_numbers.size() >= _n)
+		throw SpanFullException();
+	_numbers.push_back(number);
+}
+
+int Span::shortestSpan() const
+{
+	if (_numbers.size() <= 1)
+		throw SpanNoSpanException();
+	std::vector<int> sorted = _numbers;
+	std::sort(sorted.begin(), sorted.end());
+	int shortest = sorted[1] - sorted[0];
+	for (size_t i = 1; i < sorted.size(); i++)
+	{
+		if (sorted[i] - sorted[i - 1] < shortest)
+			shortest = sorted[i] - sorted[i - 1];
+	}
+	return shortest;
+}
+
+int Span::longestSpan() const
+{
+	if (_numbers.size() <= 1)
+		throw SpanNoSpanException();
+	std::vector<int> sorted = _numbers;
+	std::sort(sorted.begin(), sorted.end());
+	return sorted[sorted.size() - 1] - sorted[0];
+}
+
+const char* Span::SpanFullException::what() const throw()
+{
+	return "Span is full";
+}
+
+const char* Span::SpanNoSpanException::what() const throw()
+{
+	return "No span to find";
+}
+
+void Span::addRange(int start, int end)
+{
+	if (end < start)
+		throw std::invalid_argument("End must be greater than start");
+	if (_numbers.size() + end - start > _n)
+		throw SpanFullException();
+	for (int i = start; i <= end; i++)
+		_numbers.push_back(i);
+}
--- ex01/Span.hpp
+++ ex01/Span.hpp
@@ -6,7 +6,46 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 23:39:39 by akuburas          #+#    #+#             */
-/*   Updated: 2024/10/23 23:39:40 by akuburas         ###   ########.fr       */
+/*   Updated: 2024/10/23 23:56:02 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <exception>
+
+class Span
+{
+	public:
+		Span();
+		Span(unsigned int n);
+		Span(const Span& copy);
+		
+		~Span();
+		
+		Span& operator=(const Span& obj);
+		
+		void addNumber(int number);
+		void addRange(int start, int end);
+		
+		int shortestSpan() const;
+		int longestSpan() const;
+		
+		class SpanFullException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+
+		class SpanNoSpanException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+	private:
+		unsigned int _n;
+		std::vector<int> _numbers;
+};
