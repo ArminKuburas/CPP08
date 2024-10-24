e60fc2ea6bffb538f00d1b13761de615aa078f73
ArminKuburas <akuburass@gmail.com>
2024-10-24 12:07:59 +0300

    feat: Added Span main

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,74 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 23:39:46 by akuburas          #+#    #+#             */
-/*   Updated: 2024/10/23 23:39:47 by akuburas         ###   ########.fr       */
+/*   Updated: 2024/10/24 12:07:06 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Span.hpp"
+
+int	main(void)
+{
+	Span emptySpan;
+	Span span(5);
+	Span spanCopy(span);
+	Span bigSpan(10000);
+	
+	try
+	{
+		emptySpan.shortestSpan();
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	try
+	{
+		emptySpan.longestSpan();
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	try
+	{
+		span.addNumber(10);
+		span.addNumber(20);
+		span.addNumber(30);
+		span.addNumber(40);
+		span.addNumber(50);
+		//span.addNumber(60);
+		std::cout << "Shortest span: " << span.shortestSpan() << std::endl;
+		std::cout << "Longest span: " << span.longestSpan() << std::endl;
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	try
+	{
+		bigSpan.addAmount(10000);
+		std::cout << "Shortest span: " << bigSpan.shortestSpan() << std::endl;
+		std::cout << "Longest span: " << bigSpan.longestSpan() << std::endl;
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	try
+	{
+		Span sp = Span(5);
+		sp.addNumber(6);
+		sp.addNumber(3);
+		sp.addNumber(17);
+		sp.addNumber(9);
+		sp.addNumber(11);
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	return (0);
+}
